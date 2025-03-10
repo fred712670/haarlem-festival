1483ba6d7fbecee33dbe3716dbbae18662152ac6
Alaa <144346217+Alaa-nl@users.noreply.github.com>
2025-03-10 19:57:29 +0100

    Fix merge conflict in index.php

--- app/public/index.php
+++ app/public/index.php
@@ -23,12 +23,12 @@ require_once(__DIR__ . "/lib/Route.php");
  */
 require_once(__DIR__ . "/routes/index.php");
 require_once(__DIR__ . "/routes/user.php");
-<<<<<<< HEAD
+
 require_once(__DIR__ . "/routes/registrationRoute.php");
 require_once(__DIR__ . "/routes/magic.php");
 =======
 require_once(__DIR__ . "/routes/login.php");
->>>>>>> UserLogin
+
 
 // Start the router, enabling handling requests
 Route::run();
