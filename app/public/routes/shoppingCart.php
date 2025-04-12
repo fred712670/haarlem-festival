2da272b36bcca929c8d77b5a1c5d98efd0034edb
fred712670 <712670@student.inholland.nl>
2025-04-12 15:09:40 +0200

    Small code cleanup
    - New file orderRoute
    - JS of ticketValidation.php moved to assets

--- app/public/routes/shoppingCart.php
+++ app/public/routes/shoppingCart.php
@@ -8,15 +8,6 @@ Route::add('/cart', function()  {
 }, 'get');
 
 
-Route::add('/create/order', function()  {
-    $orderController = new OrderController();
-    //print_r($_SESSION['cart']);
-
-    $orderController->createOrder();
-    
-    //header('Location: /profile');
-});
-
 Route::add('/reserve', function() {
     
     //print_r($_POST);
@@ -46,35 +37,3 @@ Route::add('/deleteItem', function() {
     header('Location: /cart');
 }, 'post');
 
-Route::add('/payment', function () {
-    require_once __DIR__ . '/../views/pages/payment.php';
-}, 'get');
-
-Route::add('/process-payment', function () {
-    require_once __DIR__ . '/../controllers/OrderController.php';
-    $controller = new OrderController();
-    $controller->createOrder();
-}, 'post');
-
-Route::add('/validate/ticket', function() {
-    // Read the JSON POST input.
-    $input = file_get_contents('php://input');
-    $data = json_decode($input, true);
-    
-    if (!isset($data['qrContent'])) {
-        header('Content-Type: application/json');
-        echo json_encode(['success' => false, 'message' => 'QR content not provided.']);
-        return;
-    }
-    
-    // Instantiate the OrderController.
-    $orderController = new OrderController();
-    try {
-        $result = $orderController->validateTicket($data['qrContent']);
-        header('Content-Type: application/json');
-        echo json_encode($result);
-    } catch(Exception $e) {
-        header('Content-Type: application/json');
-        echo json_encode(['success' => false, 'message' => $e->getMessage()]);
-    }
-}, 'post');
